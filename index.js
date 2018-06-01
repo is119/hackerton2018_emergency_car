ca00a81626b58c1074ef6f5c6b1fbbd010bbe2f0
HardPlant <abc7988se@naver.com>
2018-06-01 05:16:52 -0700

    init
    Signed-off-by: HardPlant <abc7988se@naver.com>

---
+++ index.js
@@ -0,0 +1,20 @@
+let moment = require('moment')
+let cardName = "admin@hackerton-network"
+
+let composer = require('composer-client');
+let BusinessNetworkConnection = composer.BusinessNetworkConnection;
+
+let connection = new BusinessNetworkConnection();
+let definition = "";
+let factory = definition.getFactory();
+
+businessNetworkConnection.on('event', (event) => {
+    // event: { "$class": "org.namespace.BasicEvent", "eventId": "0000-0000-0000-000000#0" }
+    if(event.$class = "org.hackerton.EmergencyCarOccupied"){
+        if() // 차 ID를 먼저 받아두는 프로시저 필요
+        // 자신의 x, y와 patient_x,y에 따라 이동하는 함수
+        // 
+
+    }
+    console.log(event);
+});
