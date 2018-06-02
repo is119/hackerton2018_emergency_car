e4ae93a43390ffba17d3d58d51efcfbfd064b658
Pa-Ka <37206326+Pa-Ka@users.noreply.github.com>
2018-06-02 22:17:39 +0900

    Update event_key.js

--- event_key.js
+++ event_key.js
@@ -11,18 +11,23 @@ process.stdin.on('keypress', function (ch, key) {
     process.stdin.pause();
   }
   if (key && key.name == 'left') {
+    console.log('left send');
     http.get("http://localhost:7777/left");
   }
  if (key && key.name == 'right') {
+    console.log('right send');
     http.get("http://localhost:7777/right");
   }
  if (key && key.name == 'up') {
+    console.log('up send');
     http.get("http://localhost:7777/up");
   }
  if (key && key.name == 'down') {
+    console.log('down send');
     http.get("http://localhost:7777/down");
   }
  if (key && key.name == 'space') {
+    console.log('space send');
     http.get("http://localhost:7777/space");
   }
 });
