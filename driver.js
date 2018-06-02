5e339a7ec61ce3750457d5054ed79d9d3a7304aa
Pa-Ka <37206326+Pa-Ka@users.noreply.github.com>
2018-06-02 22:05:42 +0900

    Update driver.js

--- driver.js
+++ driver.js
@@ -19,3 +19,52 @@ exports.init = function(server){
     });
     return server
 }
+
+exports.init = function(server){
+    server.get('/space', function (req, res) {
+        pin1.value(true);
+        pin2.value(true);
+        pin3.value(true);
+        pin4.value(true);
+        
+        res.send("All stop!\n");
+    });
+    return server
+}
+
+
+exports.init = function(server){
+    server.get('/left', function (req, res) {
+        pin1.value(true);
+        //pin3.value(true);
+        res.send("Left on\n");
+    });
+    return server
+}
+
+exports.init = function(server){
+    server.get('/right', function (req, res) {
+        //pin1.value(true);
+        pin3.value(true);
+        res.send("right on\n");
+    });
+    return server
+}
+
+exports.init = function(server){
+    server.get('/up', function (req, res) {
+        pin1.value(true);
+        pin3.value(true);
+        res.send("Up on\n");
+    });
+    return server
+}
+
+exports.init = function(server){
+    server.get('/down', function (req, res) {
+        pin2.value(true);
+        pin4.value(true);
+        res.send("Down on\n");
+    });
+    return server
+}
