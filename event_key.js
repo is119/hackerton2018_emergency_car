c355c03daa583d945d61accc459e09f52a48c57c
Pa-Ka <37206326+Pa-Ka@users.noreply.github.com>
2018-06-02 22:05:36 +0900

    Update event_key.js

--- event_key.js
+++ event_key.js
@@ -3,7 +3,7 @@ let http = require('http');
 
 readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
- /*
+
 // listen for the "keypress" event
 process.stdin.on('keypress', function (ch, key) {
   console.log('got "keypress"', key);
@@ -22,25 +22,9 @@ process.stdin.on('keypress', function (ch, key) {
  if (key && key.name == 'down') {
     http.get("http://localhost:7777/down");
   }
-});
-*/
-process.stdin.on('keyup', function (ch, key) {
-  console.log('got "keyup"', key);
-  if (key && key.ctrl && key.name == 'c') {
-    process.stdin.pause();
+ if (key && key.name == 'space') {
+    http.get("http://localhost:7777/space");
   }
-  if (key && key.name == 'left') {
-    http.get("http://localhost:7777/left_up");
-  }/*
- if (key && key.name == 'right') {
-    http.get("http://localhost:7777/right_up");
-  }
- if (key && key.name == 'up') {
-    http.get("http://localhost:7777/up_up");
-  }
- if (key && key.name == 'down') {
-    http.get("http://localhost:7777/down_up");
-  }*/
 });
 
 process.stdin.setRawMode(true);
