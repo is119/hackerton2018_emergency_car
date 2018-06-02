73f43d669c360e403134ba3bc854d69c254af3a9
Pa-Ka <37206326+Pa-Ka@users.noreply.github.com>
2018-06-02 19:36:58 +0900

    Update connect.js

--- connect.js
+++ connect.js
@@ -23,6 +23,7 @@ async function main(){
 exports.init = function(server){
     server.get('/call', function (req, res) { // Recv from Driver
         http.request("http://localhost:7777/drive");
+        res.send("Hello World\n!");
     });
     return server
 }
