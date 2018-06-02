4c2dae90691e73d2760a4bf40a7108f6c9e9e4d5
Pa-Ka <37206326+Pa-Ka@users.noreply.github.com>
2018-06-02 21:58:01 +0900

    Update event_key.js

--- event_key.js
+++ event_key.js
@@ -11,9 +11,37 @@ process.stdin.on('keypress', function (ch, key) {
     process.stdin.pause();
   }
   if (key && key.name == 'left') {
-    http.get("http://localhost:7777/drive");
+    http.get("http://localhost:7777/left");
+  }
+ if (key && key.name == 'right') {
+    http.get("http://localhost:7777/right");
+  }
+ if (key && key.name == 'up') {
+    http.get("http://localhost:7777/up");
+  }
+ if (key && key.name == 'down') {
+    http.get("http://localhost:7777/down");
   }
 });
- 
+
+process.stdin.on('keyup', function (ch, key) {
+  console.log('got "keyup"', key);
+  if (key && key.ctrl && key.name == 'c') {
+    process.stdin.pause();
+  }
+  if (key && key.name == 'left') {
+    http.get("http://localhost:7777/left_up");
+  }
+ if (key && key.name == 'right') {
+    http.get("http://localhost:7777/right_up");
+  }
+ if (key && key.name == 'up') {
+    http.get("http://localhost:7777/up_up");
+  }
+ if (key && key.name == 'down') {
+    http.get("http://localhost:7777/down_up");
+  }
+});
+
 process.stdin.setRawMode(true);
 process.stdin.resume();
